32d1dcb798d95664b49fb3036a2edbba45a5a036
Thomas Draper <draper@idaccr.org>
2016-03-09 11:20:34 -0500

    Bump version: 0.1.4 → 1.0.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='qpic',
-    version='0.1.4',
+    version='1.0.0',
     description="Creating quantum circuit diagrams in TikZ",
     long_description=readme + '\n\n' + history,
     author="Sandy Kutin, Thomas Draper",
