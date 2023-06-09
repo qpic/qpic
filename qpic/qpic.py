7a1f06353f4537a7d95b9120c21fd47c4143905e
Thomas Draper <tgdrape@gmail.com>
2023-06-08 17:09:58 -0700

    Make Windows friendly.

--- qpic/qpic.py
+++ qpic/qpic.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 #############################################################################
 #           Copyright (c) 2013-16, Institute for Defense Analyses           #
 #      4850 Mark Center Drive; Alexandria, VA 22311-1882; 703-845-2500      #
